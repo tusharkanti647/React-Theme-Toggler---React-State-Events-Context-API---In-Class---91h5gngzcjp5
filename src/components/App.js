65e53528c7505c5549dd3ed05f9578f0ece20dd2
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-04-17 16:51:59 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import '../styles/App.css';
+
+const App = () => {
+
+  const [isDarkMode, setIsDarkMode] = useState();
+
+  return (
+    <div id="main">
+      <h1></h1>
+      <button></button>
+    </div>
+  )
+}
+
+
+export default App;
